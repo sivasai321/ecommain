6bff98db811427cdab9764a2de9ecc17d9a1117d
sivasai321 <sizsai142@gmail.com>
2022-12-12 10:35:22 +0530

    static files fixed

--- ecomweb/settings.py
+++ ecomweb/settings.py
@@ -141,10 +141,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL='/assets/'
-MEDIA_ROOT=BASE_DIR/'static'
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, "static"),
-]
+MEDIA_ROOT=os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS =(
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/admin')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
